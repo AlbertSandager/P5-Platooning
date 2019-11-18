e697810142fe58c41264a89e1873968fece73135
Kristian Slot <34738754+KristianSlot@users.noreply.github.com>
2019-11-18 08:18:20 +0100

    Update connectionControl.ino

--- Wireless/Wireless_full/HM10-follower/connectionControl.ino
+++ Wireless/Wireless_full/HM10-follower/connectionControl.ino
@@ -8,7 +8,7 @@ void connectionControl()  {
     ATcntrlVar = 1;
   }
 
-  delay(50);
+  delay(100);
   while (WHILEcntrl == 0) {
     for (int k = 0; k < sizeof(connectionStatus) + 1; k++)  { // do it 50 times just to be sure
       if (BTserial.available()) {
@@ -24,24 +24,41 @@ void connectionControl()  {
     Serial.println("CS isolated is:");
     Serial.println(CSisolated);
     delay(50);
-    if (CSisolated == "OK+RESET") {
-      for (int k = 0; k < sizeof(connectionStatus) + 1; k++)  { // do it 50 times just to be sure
-        if (BTserial.available()) {
+    for (int i = 0; i < sizeof(connectionStatus) + 1; i++) {
+      connectionStatus[i] = ' ';
+    }
+    while (CSisolated == "OK+RESET") {
+      Serial.println("I have read OK+reset");
+      if (BTserial.available()) {
+        for (int k = 0; k < sizeof(connectionStatus) + 1; k++)  { // do it 50 times just to be sure
           connectionStatus[k] = BTserial.read(); //fill up the receiving array
-          Serial.println("FUCK DIS NIGGA");
           Serial.print(connectionStatus[k]); //print the response from the HM-10
         }
+        String connectionStatusStr2(connectionStatus); //convert char array to string
+        char CSseperator2 = connectionStatusStr2.indexOf('O');  //set up seperator
+        char CSseperator22 = connectionStatusStr2.indexOf('T') + 1;  //set up seperator
+        CSisolated = "";
+        CSisolated = connectionStatusStr2.substring(CSseperator2, CSseperator22);
       }
-      Serial.println("");
-      if (connectionStatus == "OK+LOST")  {
-        for (int k = 0; k < sizeof(connectionStatus) + 1; k++)  { // do it 50 times just to be sure
-          if (BTserial.available()) {
+
+      delay(100);
+      while (CSisolated == "OK+LOST")  {
+        delay(50);
+        Serial.println("I have read OK+LOST");
+        if (BTserial.available()) {
+          for (int k = 0; k < sizeof(connectionStatus) + 1; k++)  { // do it 50 times just to be sure
             connectionStatus[k] = BTserial.read(); //fill up the receiving array
             Serial.print(connectionStatus[k]); //print the response from the HM-10
           }
+          String connectionStatusStr3(connectionStatus); //convert char array to string
+          char CSseperator3 = connectionStatusStr3.indexOf('O');  //set up seperator
+          char CSseperator33 = connectionStatusStr3.indexOf('T') + 1;  //set up seperator
+          CSisolated = "";
+          CSisolated = connectionStatusStr3.substring(CSseperator3, CSseperator33);
+          Serial.println(CSisolated);
         }
         Serial.println("");
-        if (connectionStatus == "OK+CONN")  {
+        if (CSisolated == "OK+CONN")  {
           Serial.println("Connection established");
           connectionbool = true;
           caseChoice = 'M';
