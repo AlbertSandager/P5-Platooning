d6c2a097295725f6008f3efd89ee577ee8d826c9
Kristian Slot <34738754+KristianSlot@users.noreply.github.com>
2019-11-28 13:38:10 +0100

    Disse 2 filer burde være good to go

--- WirelessRFtest/1-way/BT1wayRF/HM10-transmitter/HM10-transmitter.ino
+++ WirelessRFtest/1-way/BT1wayRF/HM10-transmitter/HM10-transmitter.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(11, 12);
 unsigned long starttime;
 unsigned long endtime;
 int button = 7;
@@ -10,23 +12,26 @@ void setup()
 {
   pinMode(button, INPUT_PULLUP);
   pinMode(wiredInput, INPUT);
-  Serial.begin(9600);
-  Serial.begin(9600);
-  Serial.setTimeout(30000);
+  Serial.begin(115200);
   Serial.flush();
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.flush();
 }
 
 
 void loop()
 {
-  if (choker == 0)
+  if (choker == 0 && digitalRead(button) == HIGH)
   {
+    Serial.println("Im already in here");
     String out = String('G');
-    Serial.print(out);    // sender det der blev skrevet i serial monitor over RF
-    Serial.flush();
+    mySerial.print(out);    // sender det der blev skrevet i serial monitor over RF
+    mySerial.flush();
     starttime = micros();      // take the starttime of the transmission
     choker = 1;
   }
+  
   if (digitalRead(wiredInput) == HIGH)  {
     Serial.println("Total 1-way time is: ");
     endtime = micros();                         // stop the timer
