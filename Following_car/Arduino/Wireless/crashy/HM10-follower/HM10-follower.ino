b91c96cc0a1188e927adaaeed9c450c29ddfb25f
Albert Sandager <albert@sandbo.dk>
2019-11-28 09:44:53 +0100

    everything is crashing :(

---
+++ Following_car/Arduino/Wireless/crashy/HM10-follower/HM10-follower.ino
@@ -0,0 +1,53 @@
+#include <AltSoftSerial.h>
+AltSoftSerial BTserial;
+// https://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
+
+
+//Global variables
+char caseChoice = '1';    // choice of case in switch case
+
+//variables for connectioncontrol
+char ADDRcommand[8] = "AT+ADDR?"; //the command to fetch mac adress
+char tempMacADDR[25]; // array for mac adress
+int MacADDRcontrolInt = 0;
+bool MacADDRbool = false;
+int WHILEcntrl = 0;
+
+//variables for messaging
+char messageOut[20];
+char messageIn[20];
+String MacADDRRec;   // Mac adress of HM-10
+String MacADDRConRec;   // Mac adress of HM-10
+String emergencyValRec;
+String stopbitRec;
+String startbitRec;
+String separatorRec;
+
+//Variables for communication
+char separator = ':';
+char startbit = '/'; // /
+String MacADDR;   // Mac adress of HM-10
+char MacADDRCon = 'M';
+char emergencyVal = 'N'; // N for no Y for yes
+char stopbit = '&';  // &
+
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.print("Sketch:   ");   Serial.println(__FILE__);
+  Serial.print("Uploaded: ");   Serial.println(__DATE__);
+  Serial.println(" ");
+  BTserial.begin(9600);
+  Serial.println("BTserial started at 115200");
+      MacADDRcontrol();
+  delay(100);
+
+}
+
+
+void loop() {
+
+    followerMode();
+
+}
