0054f8b8b5106e653f6544ec7547a21385dad76f
Albert Sandager <albert@sandbo.dk>
2019-12-02 08:11:18 +0100

    rf things

--- Following_car/Arduino/Wireless/RF-follower/RF-follower.ino
+++ Following_car/Arduino/Wireless/RF-follower/RF-follower.ino
@@ -53,6 +53,6 @@ void loop() {
   //printing received values
   Serial.println(addressRec);
   Serial.println(addressConRec);
-  Serial.println(emergencyVal);
+  Serial.println(emergencyValRec);
 
 }
